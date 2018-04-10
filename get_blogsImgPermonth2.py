d1ef1da51b50e2d984555585a5a0c10c5ebc6c0a
Xue <tianxueol@outlook.com>
2018-04-10 13:53:07 +0800

    add verson2.0 for python3

--- get_blogsImgPermonth2.py
+++ get_blogsImgPermonth2.py
@@ -16,14 +16,14 @@ from requests import Request, Session
 from multiprocessing.dummy import Pool
 
 
-class HandleWithinput():
+class DealWithArgv():
     def __init__(self, debug):
         self.debug = debug
         self.raw_url = ''
         self.save_path = ''
-        self.__argcCheck()
+        self.__argvCheck()
 
-    def __argcCheck(self):
+    def __argvCheck(self):
         if len(sys.argv) == 1:
             rawInput = input("Enter request blog_time year month [eg: 201710]: ")   # python3 no raw_input()
             valInput = self.__isValid(rawInput)
@@ -35,6 +35,8 @@ class HandleWithinput():
         else:
             self.__usage(sys.argv[0])
             sys.exit(-1)
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
 
     def __isValid(self, buf):
         try:
@@ -63,155 +65,156 @@ class HandleWithinput():
         print("save_path: ", self.save_path)
 
 
-
-def GetMsgEx(url, para=None, cookies=None, data=None, filename=None, debug=0):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-    if para:
-        for key in para.keys():
-            if para[key] is None:
-                del para[key]
-            else:
-                if key == 'User-Agent' and para['User-Agent'] != '':
-                    headers['User-Agent'] = para['User-Agent']
-                else:
-                    headers[key] = para[key]
-    if cookies and len(cookies) != 0:
-        headers['cookie'] = cookies
-    try:
-        if not data or len(data) == 0:
-            response = requests.get(url, headers=headers)
-        else:
-            response = response.post(url, headers=headers, data=data)
-    except Exception as e:
-        raise e
-        return None
-    if str(response.status_code)[0] != '2':
-        print( "HTTP error, status_code is %s,  url=%s"%(response.status_code, url))
-        return None
-    if debug:
-        print(url)
-        print(response.status_code)
-        pprint.pprint(headers)
-
-    if filename:
-        with open(filename, 'wb') as fd:
-            for response_data in response.iter_content(1024):
-                fd.write(response_data)
-        return filename
-    else:
-        return response.text
-
-
-class amebloImg():
+class AmebloImgReq():
     def __init__(self, para):
-        self.__paraCheck(para)
-        self.blogNum = 0
-        self.imagNum = 0
-        self.imagUrl = []
+        self.__paraGet(para)
+        self.img_url = []
+        self.blog_num = 0
+        self.imag_num = 0
+        self.imag_faild = 0
 
-    def __paraCheck(self, para):
+
+    def __paraGet(self, para):
         if not para.raw_url or not para.save_path:
             sys.exit()
         try:
             self.debug = para.debug
-            self.first_url = para.raw_url.strip()
-            self.photo_folder = para.save_path.strip()
+            self.img_dir = para.save_path.strip()
+            self.ent_url = para.raw_url.strip()
         except Exception as e:
             print(e)
             sys.exit()
 
-    def blogslistGet(self):
-        blogs_urls = []
+    def GetMsgEx(self, url, para=None, cookies=None, data=None, filename=None):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+        if para:
+            for key in para.keys():
+                if para[key] is None:
+                    del para[key]
+                else:
+                    headers[key] = para[key]
+        if cookies and len(cookies) != 0:
+            headers['cookie'] = cookies
+        try:
+            if not data or len(data) == 0:
+                response = requests.get(url, headers=headers)
+            else:
+                response = response.post(url, headers=headers, data=data)
+        except Exception as e:
+            print(e)
+            return None
+        if str(response.status_code)[0] != '2':
+            print( "HTTP error, status_code is %s,  url=%s"%(response.status_code, url))
+            return None
+        if self.debug:
+            print(url)
+            print(response.status_code)
+            pprint.pprint(headers)
 
+        if filename:
+            with open(filename, 'wb') as fd:
+                for response_data in response.iter_content(1024):
+                    fd.write(response_data)
+            return filename
+        else:
+            return response.text
+
+    def bloglistGet(self):
         headers = { 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0',
                     'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                     'Accept-Encoding':'gzip, deflate, br',
                     'Upgrade-Insecure-Requests': '1'}
+        response = self.GetMsgEx(self.ent_url, para = headers)
+        if not response:
+            print("request enter_url [{}] error!".format(self.ent_url))
+            return None
 
         try:
-            response = GetMsgEx(self.first_url, headers)
-            if not response:
-                print("request first_url[ {}] error!".format(self.first_url))
-                return None
-            # print(response.encode('utf-8'))
-
+            blogs = []
             restr = r'<ul.*?class="skin-archiveList">(.*?)</ul>'
             ret = re.findall(restr, response, re.S | re.M)
             # pattern = re.compile(restr,re.VERBOSE | re.IGNORECASE |re.DOTALL)
             # ret  = pattern.findall(response)
-            # print ret
-            if ret:
-                restr = r'<li.*?<a href="(.*?)">'
-                blogs_list = re.findall(restr, str(ret), re.S | re.M)
-                blogs_urls = [blog.strip() for blog in blogs_list if blog]
+            if not ret:
+                print("get bloglist html body failed!!")
+                return None
+            restr = r'<li.*?<a href="(.*?)">'
+            bloglist = re.findall(restr, str(ret), re.S | re.M)
+            blogs = [item.strip() for item in bloglist if item]
         except Exception as e:
             print(e)
-        self.blogNum = len(blogs_urls)
-        return blogs_urls
+        self.blog_num = len(blogs)
+        return blogs
 
-    def blogAnalyse(self, blogUrl):
-        response = GetMsgEx(blogUrl.strip())
+    def blogAnalyse(self, url):
+        response = self.GetMsgEx(url.strip())
         if not response:
-            print("request blog_url[ {} ] faild!".format(blogUrl))
-            # return (None,None,None)
+            print("request blog [{}] faild!".format(url))
+            return None
 
         # get blog title, time
 
         # get blogImg
         imgsList = self.__imagUrlGet(response)
         if not imgsList:
-            print("not get img url, blog url: ", blogUrl)
-        else:
-            self.imagNum += len(imgsList)
-            self.imagUrl.extend(imgsList)
+            print("get imag list faild!!!, raw blog url: ", url)
+            return None
+        self.imag_num += len(imgsList)
+        self.img_url.extend(imgsList)
 
     def __imagUrlGet(self, buf):
-        img_urls = []
-        restr = r'<div.*?id="entryBody"(.*?)</div>'
-        ret = re.findall(restr, buf, re.S | re.M)
-        if ret:
+        try:
+            imgs = []
+            restr = r'<div.*?id="entryBody"(.*?)</div>'
+            ret = re.findall(restr, buf, re.S | re.M)
+            if not ret:
+                print("get blog html img body failed!!!")
+                return None
             restr = r'<a.*?<img.*?src="(.*?)"'
-            img_list = re.findall(restr, str(ret), re.S | re.M)
-            img_urls = [img.strip() for img in img_list if img]
-        return img_urls
+            imglist = re.findall(restr, str(ret), re.S | re.M)
+            imgs = [img.strip() for img in imglist if img]
+        except Exception as e:
+            print(e)
+        return imgs
 
-    def imagDownload(self, url):
+
+    def imgDownload(self, url):
         try:
             urltmp = url
             urltmp = urltmp.split('/')[-1]
             imgName = urltmp.split('?')[0]
-            # print("imgName: ", imgName)
 
-            img_path = self.photo_folder.strip() + imgName.strip()
-            # print img_name
-            filename = GetMsgEx(url.strip(), filename=img_path)
+            img_path = self.img_dir.strip() + imgName.strip()
+            filename = self.GetMsgEx(url.strip(), filename=img_path)
             if not filename:
-                print("get img_src[%s] failed!"%img_src) 
-                return None 
+                print("get img_src[%s] failed!" % url)
+                self.imag_faild += 1
+                return None
             return filename
         except Exception as e:
             print(e)
             return None
 
     def myPrint(self):
-        print("debug    : ", self.debug )
-        print("raw_url  : ", self.first_url)
-        print("save_path: ", self.photo_folder)
+        print("debug  : ", self.debug )
+        print("ent_url: ", self.ent_url)
+        print("img_dir: ", self.img_dir)
 
-        print("total blogNum: ", self.blogNum)
-        print("total imagNum: ", self.imagNum)
-        print("total imagNum: ", len(self.imagUrl))
+        print("total blog_num: ", self.blog_num)
+        print("total imag_num: ", self.imag_num)
+        print("total imag_num: ", len(self.img_url))
+        print("faild imag_num: ", self.imag_faild)
 
 
 def main():
-    debug = 1
-    para = HandleWithinput(debug)
+    debug = 0
+    para = DealWithArgv(debug)
 
-    ameba = amebloImg(para)
+    ameba = AmebloImgReq(para)
 
     # get blog list
-    blogList = ameba.blogslistGet()
+    blogList = ameba.bloglistGet()
     if not blogList:
         print("get  blogs url list failed!!!")
         sys.exit()
@@ -224,12 +227,14 @@ def main():
 
     # download img
     pool = Pool(20)
-    results = pool.map(ameba.imagDownload, ameba.imagUrl)
+    results = pool.map(ameba.imgDownload, ameba.img_url)
     pool.close()
     pool.join()
     for item in results:
         if not item:
             print("Download {} failed!!!!!!".format(item))
+    ameba.myPrint()
+
 
 if __name__ == '__main__':
     main()
